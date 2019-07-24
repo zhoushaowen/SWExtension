52e8090b0a1c3b9de95ba70b309f2d8e7c491f59
周少文 <348345883@qq.com>
2019-07-24 09:16:42 +0800

    优化身份证真实性校验

--- SWExtension/SWExtension/Core/Foundation/NSString+SWValid.m
+++ SWExtension/SWExtension/Core/Foundation/NSString+SWValid.m
@@ -39,7 +39,7 @@
 //        BOOL isMatch2 = [pred2 evaluateWithObject:number];
 //        NSPredicate *pred3 = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT_NUM];
 //        BOOL isMatch3 = [pred3 evaluateWithObject:number];
-//        
+//
 //        if (isMatch1 || isMatch2 || isMatch3) {
 //            return YES;
 //        }else{
@@ -56,25 +56,153 @@
 }
 
 - (BOOL)sw_isValidateIdentityCard {
-    if([self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]].length < 1) return NO;
-    NSString *regex2 = @"^(\\d{14}|\\d{17})(\\d|[xX])$";
-    NSPredicate *identityCardPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex2];
-    BOOL valid = [identityCardPredicate evaluateWithObject:self];
-    if(!valid) return valid;
-    //验证出身月份和生日日期是否合法
-    if(self.length == 18){
-        NSInteger month = [[self substringWithRange:NSMakeRange(10, 2)] integerValue];
-        NSInteger day = [[self substringWithRange:NSMakeRange(12, 2)] integerValue];
-        if(month < 13 && month > 0 && day < 32 && day > 0) return YES;
-        return NO;
-    }else if (self.length == 15){
-        NSInteger month = [[self substringWithRange:NSMakeRange(8, 2)] integerValue];
-        NSInteger day = [[self substringWithRange:NSMakeRange(10, 2)] integerValue];
-        if(month < 13 && month > 0 && day < 32 && day > 0) return YES;
+    //    if([self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]].length < 1) return NO;
+    //    NSString *regex2 = @"^(\\d{14}|\\d{17})(\\d|[xX])$";
+    //    NSPredicate *identityCardPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex2];
+    //    BOOL valid = [identityCardPredicate evaluateWithObject:self];
+    //    if(!valid) return valid;
+    //    //验证出身月份和生日日期是否合法
+    //    if(self.length == 18){
+    //        NSInteger month = [[self substringWithRange:NSMakeRange(10, 2)] integerValue];
+    //        NSInteger day = [[self substringWithRange:NSMakeRange(12, 2)] integerValue];
+    //        if(month < 13 && month > 0 && day < 32 && day > 0) return YES;
+    //        return NO;
+    //    }else if (self.length == 15){
+    //        NSInteger month = [[self substringWithRange:NSMakeRange(8, 2)] integerValue];
+    //        NSInteger day = [[self substringWithRange:NSMakeRange(10, 2)] integerValue];
+    //        if(month < 13 && month > 0 && day < 32 && day > 0) return YES;
+    //        return NO;
+    //    }
+    //    return NO;
+    //判断位数
+    if ([self length] != 15 && [self length] != 18) {
         return NO;
     }
-    return NO;
+    
+    long lSumQT =0;
+    //加权因子
+    int R[] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
+    
+    //校验码
+    unsigned char sChecker[11]={'1','0','X', '9', '8', '7', '6', '5', '4', '3', '2'};
+    
+    NSString *carid = [self copy];
+    
+    
+    //将15位身份证号转换成18位
+    NSMutableString *mString = [NSMutableString stringWithString:self];
+    
+    if ([self length] == 15) {
+        
+        [mString insertString:@"19" atIndex:6];
+        long p = 0;
+        const char *pid = [mString UTF8String];
+        for (int i=0; i<=16; i++)
+        {
+            p += (pid[i]-48) * R[i];
+        }
+        
+        int o = p%11;
+        NSString *string_content = [NSString stringWithFormat:@"%c",sChecker[o]];
+        [mString insertString:string_content atIndex:[mString length]];
+        carid = mString;
+    }
+    
+    //判断地区码
+    NSString * sProvince = [carid substringToIndex:2];
+    if (![NSString sw_checkAreaCode:sProvince]) {
+        return NO;
+    }
+    //判断年月日是否有效
+    //年份
+    int strYear = [[carid substringWithRange:NSMakeRange(6,4)] intValue];
+    
+    
+    //月份
+    int strMonth = [[carid substringWithRange:NSMakeRange(10,2)] intValue];
+    //日
+    int strDay = [[carid substringWithRange:NSMakeRange(12,2)] intValue];
+    
+    NSTimeZone *localZone = [NSTimeZone localTimeZone];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+    [dateFormatter setTimeZone:localZone];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSDate *date=[dateFormatter dateFromString:[NSString stringWithFormat:@"%d-%d-%d 12:01:01",strYear,strMonth,strDay]];
+    
+    if (date == nil) {
+        return NO;
+    }
+    const char *PaperId  = [carid UTF8String];
+    //检验长度
+    if( 18 != strlen(PaperId)) return -1;
+    //校验数字
+    for (int i=0; i<18; i++)
+    {
+        if ( !isdigit(PaperId[i]) && !(('X' == PaperId[i] || 'x' == PaperId[i]) && 17 == i) )
+        {
+            return NO;
+        }
+    }
+    //验证最末的校验码
+    for (int i=0; i<=16; i++)
+    {
+        lSumQT += (PaperId[i]-48) * R[i];
+    }
+    if (sChecker[lSumQT%11] != PaperId[17] )
+    {
+        return NO;
+    }
+    return YES;
+    
 }
+
++ (BOOL)sw_checkAreaCode:(NSString *)code
+{
+    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+    [dic setObject:@"北京" forKey:@"11"];
+    [dic setObject:@"天津" forKey:@"12"];
+    [dic setObject:@"河北" forKey:@"13"];
+    [dic setObject:@"山西" forKey:@"14"];
+    [dic setObject:@"内蒙古" forKey:@"15"];
+    [dic setObject:@"辽宁" forKey:@"21"];
+    [dic setObject:@"吉林" forKey:@"22"];
+    [dic setObject:@"黑龙江" forKey:@"23"];
+    [dic setObject:@"上海" forKey:@"31"];
+    [dic setObject:@"江苏" forKey:@"32"];
+    [dic setObject:@"浙江" forKey:@"33"];
+    [dic setObject:@"安徽" forKey:@"34"];
+    [dic setObject:@"福建" forKey:@"35"];
+    [dic setObject:@"江西" forKey:@"36"];
+    [dic setObject:@"山东" forKey:@"37"];
+    [dic setObject:@"河南" forKey:@"41"];
+    [dic setObject:@"湖北" forKey:@"42"];
+    [dic setObject:@"湖南" forKey:@"43"];
+    [dic setObject:@"广东" forKey:@"44"];
+    [dic setObject:@"广西" forKey:@"45"];
+    [dic setObject:@"海南" forKey:@"46"];
+    [dic setObject:@"重庆" forKey:@"50"];
+    [dic setObject:@"四川" forKey:@"51"];
+    [dic setObject:@"贵州" forKey:@"52"];
+    [dic setObject:@"云南" forKey:@"53"];
+    [dic setObject:@"西藏" forKey:@"54"];
+    [dic setObject:@"陕西" forKey:@"61"];
+    [dic setObject:@"甘肃" forKey:@"62"];
+    [dic setObject:@"青海" forKey:@"63"];
+    [dic setObject:@"宁夏" forKey:@"64"];
+    [dic setObject:@"新疆" forKey:@"65"];
+    [dic setObject:@"台湾" forKey:@"71"];
+    [dic setObject:@"香港" forKey:@"81"];
+    [dic setObject:@"澳门" forKey:@"82"];
+    [dic setObject:@"国外" forKey:@"91"];
+    
+    if ([dic objectForKey:code] == nil) {
+        return NO;
+    }
+    return YES;
+}
+
 
 - (NSString *)sw_parseBirthdayFromIdentityCard {
     if(![self sw_isValidateIdentityCard]) return nil;
